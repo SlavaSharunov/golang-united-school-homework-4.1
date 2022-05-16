b4031e1f8782eb1c3d995a11faa4f5be03380c98
Viacheslav Sharunov <sharunov@mst.ch>
2022-05-16 12:53:01 +0200

    Solution for homework 4.1

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,9 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	runes := []rune(input)
+	for r := len(runes) - 1; r >= 0; r-- {
+		output += string(runes[r])
+	}
 	return output
 }
